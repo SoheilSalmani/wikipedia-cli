dc69d2459ff8531bb76a632bd65b66534fc7d97c
Soheil Salmani <salmani.soheil@gmail.com>
2021-09-16 01:00:29 +0000

    ✅ test(wikipedia): add tests for `wikipedia` module
    Add several test cases for the `wikipedia` module (2 unit tests).

---
+++ tests/test_wikipedia.py
@@ -0,0 +1,17 @@
+import pytest
+
+from wikipedia_cli import wikipedia
+
+@pytest.fixture
+def mock_requests_get(mocker):
+   return mocker.patch("requests.get")
+
+def test_get_random_uses_en_wikipedia_domain_by_default(mock_requests_get):
+    wikipedia.get_random()
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+def test_get_random_uses_specified_wikipedia_domain(mock_requests_get):
+    wikipedia.get_random(lang="de")
+    args, _ = mock_requests_get.call_args
+    assert "de.wikipedia.org" in args[0]
