ff2147f873a8f0df783e816ae5ef258369939ae2
Soheil Salmani <salmani.soheil@gmail.com>
2021-09-16 05:02:51 +0000

    🔨 chore: configure Nox and add Poetry compatibility
    Create the Nox configuration file and add compatibility with Poetry in order to
    use Poetry for package installations instead of pip.

---
+++ noxfile.py
@@ -0,0 +1,51 @@
+import contextlib
+import tempfile
+
+import nox
+
+package = "wikipedia_cli"
+
+class Poetry:
+    def __init__(self, session):
+        self.session = session
+
+    @contextlib.contextmanager
+    def export(self, *args):
+        with tempfile.NamedTemporaryFile() as requirements:
+            self.session.run(
+                "poetry",
+                "export",
+                *args,
+                "--format=requirements.txt",
+                f"--output={requirements.name}",
+                "--without-hashes",
+                external=True,
+            )
+            yield requirements.name
+
+    def version(self):
+        output = self.session.run(
+            "poetry", "version", external=True, silent=True
+        )
+        return output.split()[1]
+
+    def build(self, *args):
+        self.session.run("poetry", "build", *args, external=True, silent=True)
+
+def install_package(session):
+    poetry = Poetry(session)
+
+    with poetry.export() as requirements:
+        session.install(f"--requirement={requirements}")
+
+    poetry.build("--format=wheel")
+
+    version = poetry.version()
+    session.install(
+        "--no-deps", "--force-reinstall", f"dist/{package}-{version}-py3-none-any.whl"
+    )
+
+def install(session, *args):
+    poetry = Poetry(session)
+    with poetry.export("--dev") as requirements:
+        session.install(f"--constraint={requirements}", *args)
